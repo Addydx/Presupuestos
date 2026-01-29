03b6fa76712f50b8c6b4d3da8b98af974fca171a
alexmendezruiz <japenamie@gmail.com>
2026-01-28 21:11:10 -0600

    feat: chambeando en el login

---
+++ presupuesto_app/lib/screens/auth/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  String errorMessage = '';
+
+  void login() {
+    final email = emailController.text;
+    final password = passwordController.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: 'Contraseña'),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: login,
+              child: const Text('Iniciar Sesion'),
+            ),
+            const SizedBox(height: 10),
+            Text(errorMessage, style: const TextStyle(color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+}
