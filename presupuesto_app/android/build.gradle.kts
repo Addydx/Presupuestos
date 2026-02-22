7f48c293a594aa7bc77a0613ad6d7da4dd63824d
alexmendezruiz <japenamie@gmail.com>
2026-02-21 21:48:17 -0600

    avances

--- presupuesto_app/android/build.gradle.kts
+++ presupuesto_app/android/build.gradle.kts
@@ -8,6 +8,12 @@ allprojects {
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+plugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
